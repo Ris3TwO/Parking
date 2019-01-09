c540e2171d7a93ae932164c4c42fe9dc5156cc3d
Ris3TwO <Ris3TwO@Ris3TwO-PC>
2019-01-08 23:51:47 -0300

    Initial Release

---
+++ src/mvc/home.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+/**
+ *
+ * @author Ris3TwO
+ */
+public class home {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
